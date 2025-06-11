a0a177aea241154e1448cc61d1e23c2b9dd0eac6
mchemrl <mcchemro@gmail.com>
2025-06-11 13:11:23 +0300

    tf

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -49,7 +49,7 @@ const handleLoginClick = async (e) => {
 };
 
   const handleGoogleLogin = () => {
-    window.location.href = "/login/google";
+    window.location.href = "http://127.0.0.1:5000/auth/login/google";
   };
 
   return (
